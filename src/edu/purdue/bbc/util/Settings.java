d19ba35fe959eedc6bb0e79ab53c29aa8fa86c64
Thomas McGrew <tmcgrew@purdue.edu>
2011-10-17 17:37:19 -0400

    Added defaultable methods to Settings Added API documentation to Settings

--- src/edu/purdue/bbc/util/Settings.java
+++ src/edu/purdue/bbc/util/Settings.java
@@ -48,22 +48,48 @@ public class Settings extends Properties {
 	private static Language language;
 	private String settingsFilename;
 
+	/**
+	 * Creates a new Settings object.
+	 */
 	public Settings( ) {
 		this((Properties)null, null );
 	}
 
+	/**
+	 * Creates a new Settings object, reading settings from the given file.
+	 * 
+	 * @param settingsFile The file to read previously saved settings from.
+	 */
 	public Settings( String settingsFile ) {
 		this( (Properties)null, settingsFile );
 	}
 
+	/**
+	 * Creates a new Settings object with the passed in default settings.
+	 * 
+	 * @param defaultSettings A Properties object to be used for default values.
+	 */
 	public Settings( Properties defaultSettings ) {
 		this( defaultSettings, null );
 	}
 
+	/**
+	 * Creates a new Sttings object with the passed in default settings.
+	 * 
+	 * @param defaultSettings A Map containing settings to be used for default 
+	 *	values.
+	 */
 	public Settings( Map<String,String> defaultSettings ) {
 		this( defaultSettings, null );
 	}
 
+	/**
+	 * Creates a new Settings object with the passed in default settings and
+	 * default values.
+	 * 
+	 * @param defaultSettings A Properties object to be used for default values.
+	 * @param settingsFile The file to read previously saved settings from.
+	 */
 	public Settings( Properties defaultSettings, String settingsFile ) {
 		super( );
 		this.defaults = new DefaultSettings( );
@@ -84,6 +110,14 @@ public class Settings extends Properties {
 		}
 	}
 
+	/**
+	 * Creates a new Settings object with the passed in default settings and
+	 * default values.
+	 * 
+	 * @param defaultSettings a Map containing settings to be used for default 
+	 *	values.
+	 * @param settingsFile The file to read previously saved settings from.
+	 */
 	public Settings( Map<String,String> defaultSettings, String settingsFile ) {
 		super( );
 		this.defaults = new DefaultSettings( );
@@ -101,10 +135,19 @@ public class Settings extends Properties {
 		}
 	}
 
+	/**
+	 * Sets the specified property to the specified value.
+	 * 
+	 * @param property The name of property to be set.
+	 * @param value The value to set the property to.
+	 * @return The previous value of the specified property, or null if there
+	 *	is not one.
+	 */
+	@Override
 	public Object setProperty( String property, String value ) {
 		Object returnValue = super.setProperty( property, value );
 		if ( "locale".equals( property ))
-			if ( this.language == null )
+			if ( language == null )
 				language = new Language( value );
 			else
 				language.setLocale( value );
@@ -112,48 +155,200 @@ public class Settings extends Properties {
 		return returnValue;
 	}
 
+	/**
+	 * Gets the initial settings object, usually the first one created by the
+	 * application.
+	 * 
+	 * @return The default settings object.
+	 */
 	public static Settings getSettings( ) {
 		return settings;
 	}
 
+	/**
+	 * Gets the Language which associated with the locale specified in
+	 * the settings. Creates a new empty Language if one is not present.
+	 * 
+	 * @return The current Language.
+	 */
 	public static Language getLanguage( ) {
 		if ( language == null )
 			language = new Language( );
 		return language;
 	}
 
+	/**
+	 * Gets the default settings specified at the creation of this Settings
+	 * object, if any.
+	 * 
+	 * @return The default settings.
+	 */
 	public Properties getDefaults( ){
 		return this.defaults;
 	}
 
+	/**
+	 * Returns the value of the property as a boolean.
+	 * 
+	 * @param property The name of the property to retrieve.
+	 * @return A boolean containing the value of this property.
+	 */
 	public boolean getBoolean( String property ) {
-		return Boolean.parseBoolean( get( property ));
+		return Boolean.parseBoolean( this.get( property ));
 	}
 
+	/**
+	 * Returns the value of the property as a boolean, or the default value
+	 * specified is this property is not set.
+	 * 
+	 * @param property The name of the property to retrieve.
+	 * @param defaultValue The default value to return if the property is not set.
+	 * @return A boolean containing the value of this property.
+	 */
+	public boolean getBoolean( String property, boolean defaultValue ) {
+		String returnValue = this.get( property );
+		if ( returnValue == null )
+			return defaultValue;
+		else
+			return Boolean.parseBoolean( returnValue );
+	}
+
+	/**
+	 * Returns the value of the property as an int.
+	 * 
+	 * @param property the name of the property to retrieve.
+	 * @return An int containing the value of this property.
+	 * @throws NumberFormatException if this property does not contain a valid
+	 *	integer.
+	 */
 	public int getInt( String property ) {
-		return Integer.parseInt( get( property ));
+		return Integer.parseInt( this.get( property ));
 	}
 
+	/**
+	 * Returns the value of the property as an int, or the default value
+	 * specified if this property is not set.
+	 * 
+	 * @param property The name of the property to retrieve.
+	 * @param devaultValue The default value to return if the property is not set.
+	 * @return An int containing the value of this property.
+	 * @throws NumberFormatException if this property does not contain a valid
+	 *	integer.
+	 */
+	public int getInt( String property, int defaultValue ) {
+		String returnValue = this.get( property );
+		if ( returnValue == null )
+			return defaultValue;
+		else
+			return Integer.parseInt( returnValue );
+	}
+
+	/**
+	 * Returns the value of the property as a double.
+	 * 
+	 * @param property The name of the property to retrieve.
+	 * @return A double containing the value of this property.
+	 * @throws NumberFormatException if this property does not contain a valid
+	 *	double.
+	 */
 	public double getDouble( String property ) {
-		return Double.parseDouble( get( property ));
+		return Double.parseDouble( this.get( property ));
 	}
 
+	/**
+	 * Returns the value of the property as a double, or the default value
+	 * specified if this property is not set.
+	 * 
+	 * @param property The name of the property to retrieve.
+	 * @param defaultValue The default value to return if the property is not set.
+	 * @return A double containing the value of this property.
+	 * @throws NumberFormatException if this property does not contain a valid
+	 *	double.
+	 */
+	public double getDouble( String property, double defaultValue ) {
+		String returnValue = this.get( property );
+		if ( returnValue == null )
+			return defaultValue;
+		else
+			return Double.parseDouble( returnValue );
+	}
+
+	/**
+	 * Returns the value of the property.
+	 * 
+	 * @param The name of the property to retrieve.
+	 * @return The value of the specified property.
+	 */
 	public String get( String property ) {
-		return getProperty( property );
+		return this.getProperty( property );
 	}
 
-	public void setBoolean( String property, boolean value ) {
-		setProperty( property, Boolean.toString( value ));
+	/**
+	 * Returns the value of the property, or the default value specified if
+	 * the property is not set.
+	 * 
+	 * @param property The name of the property to retrieve.
+	 * @param defaultValue The default value to return if the property is not set.
+	 * @return A String containing the value of this property.
+	 */
+	public String get( String property, String defaultValue ) {
+		return this.getProperty( property, defaultValue );
 	}
 
-	public void setInt( String property, int value ) throws NumberFormatException {
-		setProperty( property, Integer.toString( value ));
+	/**
+	 * Sets the value of the specified property to the specified boolean value.
+	 * 
+	 * @param property The name of the property to be set.
+	 * @param value The value to set the property to.
+	 * @return The previous value of this property, if any, false otherwise.
+	 */
+	public boolean setBoolean( String property, boolean value ) {
+		Object returnValue = this.setProperty( property, Boolean.toString( value ));
+		if ( returnValue == null )
+			return false;
+		else
+			return Boolean.parseBoolean( returnValue.toString( ));
 	}
 
-	public void setDouble( String property, double value ) throws NumberFormatException {
-		setProperty( property, Double.toString( value ));
+	/**
+	 * Sets the value of the specified property to the specified int value.
+	 * 
+	 * @param property The name of the property to be set.
+	 * @param value The value to set this property to.
+	 * @return The previous value of this property, if any, -1 otherwise.
+	 * @throws NumberFormatException if the previous value of this property is not
+	 *	null and does not contain a valid integer.
+	 */
+	public int setInt( String property, int value ) throws NumberFormatException {
+		Object returnValue = this.setProperty( property, Integer.toString( value ));
+		if ( returnValue == null )
+			return -1;
+		else
+			return Integer.parseInt( returnValue.toString( ));
 	}
 
+	/**
+	 * Sets the value of the specifeid property to the specified double value.
+	 * 
+	 * @param property The name of the property to be set.
+	 * @param value The value to set this property to.
+	 * @return The previous value of this property, if any, Double.NaN othersise.
+	 * @throws NumberFormatException if the previous value of this property is not
+	 *	null and does not contain a valid double.
+	 */
+	public double setDouble( String property, double value ) throws NumberFormatException {
+		Object returnValue = this.setProperty( property, Double.toString( value ));
+		if ( returnValue == null )
+			return Double.NaN;
+		else
+			return Integer.parseInt( returnValue.toString( ));
+	}
+
+	/**
+	 * Saves the settings to the output file specified in the constructor.
+	 * 
+	 * @return true if saving of the file was successful.
+	 */
 	public boolean save( ) {
 
 		Logger logger = Logger.getLogger( getClass( ));
@@ -180,6 +375,11 @@ public class Settings extends Properties {
 		return true;
 	}
 	
+	/**
+	 * Loads the settings from the file specified in the constructor.
+	 * 
+	 * @return true if loading of the settings was successful.
+	 */
 	public boolean load( ) {
 		Logger logger = Logger.getLogger( getClass( ));
 		if ( this.settingsFilename == null ) {
