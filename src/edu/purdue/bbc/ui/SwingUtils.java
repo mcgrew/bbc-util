5b0e1d8453a62e722201f30becae4b456936fd62
Thomas McGrew <tmcgrew@purdue.edu>
2013-08-22 18:49:44 -0400

    Bug fix for SwingUtils.getRealLocation()

--- src/edu/purdue/bbc/ui/SwingUtils.java
+++ src/edu/purdue/bbc/ui/SwingUtils.java
@@ -7,6 +7,23 @@ import java.awt.*;
 public class SwingUtils {
 
   /**
+   * Finds the real location of the component relative to the Frame
+   *
+   * @param container The container to find the bounds for.
+   * @return The real bounds of the container.
+   */
+  public static Point getRealLocation(Container container) {
+    Point point = container.getLocation();
+    for (Container c = container.getParent(); !(c instanceof Window);
+         c = c.getParent()) {
+      Point p = c.getLocation();
+      point.x += p.x;
+      point.y += p.y;
+    }
+    return point;
+  }
+
+  /**
    * Finds the real bounds of the component relative to the root Window
    *
    * @param container The container to find the bounds for.
@@ -26,8 +43,8 @@ public class SwingUtils {
    */
   public static Rectangle getRealBounds(Container container, Rectangle rect) {
     container.getBounds(rect);
-
-    for (Container c = container.getParent(); c != null; c = c.getParent()) {
+    for (Container c = container.getParent(); !(c instanceof Window);
+         c = c.getParent()) {
       Point p = c.getLocation();
       rect.x += p.x;
       rect.y += p.y;
